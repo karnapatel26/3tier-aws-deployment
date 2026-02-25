0fdab19721329ac8fbba180e08fa4552640a84f3
karnapatel26 <12202040601077@adit.ac.in>
2026-02-25 12:26:18 +0530

    Force redeploy test

--- nodejs-express-mysql/server.js
+++ nodejs-express-mysql/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to karna's application." });
 });
 
 require("./app/routes/tutorial.routes.js")(app);
